e7ca53215884e57bda4210d859b96fb343f4e87b
m2430132_aix <miyazato@uec.ac.jp>
2025-06-25 15:36:08 +0900

    add systhesized_data

--- scripts/generate_instructions_jp.sh
+++ scripts/generate_instructions_jp.sh
@@ -1,9 +1,9 @@
-batch_dir=data/Qwen3-30B-A3B/
+batch_dir=data/Qwen3-14B/
 
 nohup python self_instruct/self_instaruct_japanese.py \
     --batch_dir ${batch_dir} \
     --num_instructions_to_generate 5000 \
     --seed_tasks_path ichikara_data/merged_output.jsonl \
     --engine "davinci" \
-    --model_name "Qwen/Qwen3-30B-A3B" \
-    &
+    --model_name "Qwen/Qwen3-14B" \
+    >> logs/qwen3_14B.logs & 
